e7f7852dc6c13f631519f4b5a07792b62cd5cbca
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-06-03 23:46:16 +0500

    api

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,29 @@
+import  {NextResponse,NextRequest} from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+
+
+
+export async function POST(request:NextRequest){
+    try {
+        const {email,password} = await request.json();
+        if(!email || !password){
+            return NextResponse.json({message:"Email or password is missing"},{status:400});
+        }
+
+        await connectToDatabase();
+
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return NextResponse.json({message:"User already exists"},{status:400});
+        }
+
+        await User.create({email,password});
+
+        return NextResponse.json({message:"User created successfully"},{status:201});
+
+    } catch (error) {
+        return NextResponse.json({message:"Something went wrong from register"},{status:500});
+        
+    }
+}
